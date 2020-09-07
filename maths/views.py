6607f66071ee6e3027ca572eb0c918e07dd7a9aa
Adrian Jelonek <ajelonek@gmail.com>
2020-09-07 22:32:51 +0200

    Finished project dingo - super basic web calculator

--- maths/views.py
+++ maths/views.py
@@ -9,18 +9,46 @@ def math(request):
 
 
 def add(request, a, b):
-    return HttpResponse(a + b)
+    a, b = int(a), int(b)
+    wynik = a + b
+    c = {'a': a, 'b': b, 'operacja': '+', 'wynik': wynik, 'title': 'dodawanie'}
+    return render(
+        request=request,
+        template_name="maths/operation.html",
+        context=c
+    )
 
 
 def sub(request, a, b):
-    return HttpResponse(a - b)
+    a, b = int(a), int(b)
+    wynik = a - b
+    c = {'a': a, 'b': b, 'operacja': '-', 'wynik': wynik, 'title': 'odejmowanie'}
+    return render(
+        request=request,
+        template_name="maths/operation.html",
+        context=c
+    )
 
 
 def mul(request, a, b):
-    return HttpResponse(a * b)
+    a, b = int(a), int(b)
+    wynik = a * b
+    c = {'a': a, 'b': b, 'operacja': '*', 'wynik': wynik, 'title': 'mnożenie'}
+    return render(
+        request=request,
+        template_name="maths/operation.html",
+        context=c
+    )
 
 
 def div(request, a, b):
+    a, b = int(a), int(b)
     if b == 0:
-        return HttpResponse("Nie dziel przez 0")
-    return HttpResponse(a / b)
+        return HttpResponse('Nie dziel przez zero!')
+    wynik = a / b
+    c = {'a': a, 'b': b, 'operacja': '/', 'wynik': wynik, 'title': 'dzielenie'}
+    return render(
+        request=request,
+        template_name="maths/operation.html",
+        context=c
+    )
